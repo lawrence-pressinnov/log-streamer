ee97d7eaf385a080325b39f9b8de792649422cd9
Lawrence Carbon <lawrence.carbon@pressinnov.com>
2016-10-12 00:17:02 +0200

    Manage args or env at server startup Improve UI First implementation of log filtering (not really functional but working already)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var socketio = require('socket.io');
 var logger = require('winston');
 var mongo = require('mongodb');
+var program = require('commander');
 
 var websocket;
 
@@ -11,17 +12,36 @@ var websocket;
 // var mongoCollection = "log";
 // var serverPort = "8080";
 
-var mongoUri = process.env.MONGO_URI;
-var mongoCollection = process.env.MONGO_COLLECTION;
-var serverPort = process.env.PORT;
+var mongoUri = null;
+var mongoCollection = null;
+var serverPort = null;
 
 function checkParams(){
 
-  logger.info(process.env.MONGO_URI);  
-  logger.info(process.env.MONGO_COLLECTION);  
-  logger.info(process.env.PORT);  
+  if (program.mongoUri != null){
+    mongoUri = program.mongoUri;
+  } else if(process.env.MONGO_URI != null) {
+    mongoUri = process.env.MONGO_URI;
+  }
 
-  if(process.env.MONGO_URI && process.env.MONGO_COLLECTION && process.env.PORT)
+  if (program.mongoCollection != null){
+    mongoCollection = program.mongoCollection;
+  } else if(process.env.MONGO_COLLECTION != null) {
+    mongoCollection = process.env.MONGO_COLLECTION;
+  }
+
+  if (program.port != null){
+    serverPort = program.port;
+  } else if(process.env.PORT != null) {
+    serverPort = process.env.PORT;
+  }
+
+
+  logger.info(mongoUri);  
+  logger.info(mongoCollection);  
+  logger.info(serverPort);  
+
+  if(mongoUri && mongoCollection && serverPort)
     return true;
 
 }
@@ -32,17 +52,29 @@ var subscribe = function(){
   var next = args.pop();
   var filter = args.shift() || {};
 
+  //console.info(next + ' ' + filter);
+
   if('function' !== typeof next) throw('Callback function not defined');
 
  // connect to MongoDB
-  mongo.MongoClient.connect(mongoUri, function(err, db){
-
+  mongo.MongoClient.connect("mongodb://" + mongoUri, function(err, db){
+      
+      if(err)
+        throw('db err: ' + err);
+      
    // make sure you have created capped collection "messages" on db "test"
     db.collection(mongoCollection, function(err, coll) {
+
+      if(err)
+        throw('collection err: ' + err);
 
      // seek to latest object
       var seekCursor = coll.find(filter).limit(1);//.sort({$natural: -1}).limit(1);
       seekCursor.nextObject(function(err, latest) {
+
+        if(err)
+          throw('seek err: ' + err);
+      
         if (latest) {
           filter._id = { $gt: latest._id }
         }
@@ -64,19 +96,35 @@ var subscribe = function(){
   });     
 };
 
-if(!checkParams()){
-  process.exit(1);
-}
-
-// new documents will appear in the console
-subscribe( function(document) {
+var emitMessages = function(document){
   if(websocket){
     logger.info(document);  
     websocket.emit('message',document);
   } else {
     logger.info("nobody is listening");
   }
-});
+};
+
+program
+.version('0.0.1')
+.option('-p, --port [port]', 'The HTTP port used')
+.option('-m, --mongoUri [uri]', 'The MongoDB host and port like 127.0.0.1:27017')
+.option('-c, --mongoCollection [collection]', 'The MongoDB collection to tail')
+.parse(process.argv);
+  
+
+if(!checkParams()){
+  process.exit(1);
+}
+
+subscribe(emitMessages);
+
+var filterStream = function(q){
+
+  console.info("filter received: " + q);
+  subscribe(JSON.parse(q), emitMessages);
+
+};
 
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
@@ -93,6 +141,8 @@ io.sockets.on('connection', function (socket) {
     logger.info('Un client est connecté !');
     //enregistre la connexion ouverte avec le client
     websocket = socket;
+
+    websocket.on('filter',filterStream);
 });
 
 server.listen(serverPort);
